547b3c394947493f12dd1e6662365e18091e6d48
Iyanu Arowosola <iarowosola@yahoo.com>
2025-09-11 15:16:24 +0100

    homepage

--- src/Components/CarouselImages.js
+++ src/Components/CarouselImages.js
@@ -1,7 +1,8 @@
-import Banner1 from "../Assets/Images/banner1.jpg";
+// import Banner1 from "../Assets/Images/banner1.jpg";
 import Banner2 from "../Assets/Images/banner2.jpg";
 import Banner3 from "../Assets/Images/banner3.jpg";
 import Banner4 from "../Assets/Images/banner4.jpg";
+import Banner5 from "../Assets/Images/banner5.jpg";
 
 
 export default [
@@ -14,6 +15,11 @@ export default [
     title: "Congregation",
     content: "",
     backgroundImage: Banner2,
+    {
+      title: "Campus-meeting",
+      content: "",
+      backgroundImage: Banner5,
+    },
   },
   {
     title: "Congregation",
